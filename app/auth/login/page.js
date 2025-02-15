96adf418aad4eee917bbb189a433d97b5740ac90
logesh <logeshkumarmurugan98@gmail.com>
2025-02-15 16:57:26 +0530

    login

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,10 +1,12 @@
 "use client";
 import React, { useState } from "react";
 import styles from "../../scss/comp/auth.module.scss"; 
-
+import { useRouter } from 'next/navigation';
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const router = useRouter();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -13,22 +15,39 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Logging in with:", formData);
-    // Add authentication logic here
+  };
+
+    const handleForgotPassword = () => {
+    router.push("/auth/forgot_password")
   };
 
   return (
     <div className={styles.container}>
+      <img src="/download.png" alt="logo" className={styles.logo} />
       <form onSubmit={handleSubmit} className={styles.loginForm}>
         <h2>Login</h2>
         <div className={styles.inputGroup}>
           <label>Email:</label>
-          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+          <input 
+            type="email" 
+            name="email" 
+            value={formData.email} 
+            onChange={handleChange} 
+            required 
+          />
         </div>
         <div className={styles.inputGroup}>
           <label>Password:</label>
-          <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+          <input 
+            type="password" 
+            name="password" 
+            value={formData.password} 
+            onChange={handleChange} 
+            required 
+          />
         </div>
         <button type="submit" className={styles.loginButton}>Login</button>
+        <a onClick={handleForgotPassword}>Forgot Password?</a>
       </form>
     </div>
   );
